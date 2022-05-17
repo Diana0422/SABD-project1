9faabb7969a41821b4517baccf4f3e1a445c24be
Giacomo <giacomo.redjack@gmail.com>
2022-05-17 22:11:59 +0200

    Prima query funzionante

--- src/main/java/com/sparkling_taxi/Query1.java
+++ src/main/java/com/sparkling_taxi/Query1.java
@@ -19,107 +19,26 @@ public class Query1 {
     public static final String OUT_DIR = "hdfs://namenode:9000/home/dataset-batch/output-query1/";
     public static final int PASSENGER_COUNT_COL = 3;
 
-
-    public static double passengerMean(JavaRDD<Double> passengers) {
-        // actions that counts the number of elements in the java rdd
-        long num = passengers.count();
-        System.out.println(num);
-        // action that sums each value in the rdd
-        double sum = passengers.reduce(Double::sum);
-        System.out.println(sum);
-        return (double) sum / (double) num;
+    /**
+     * @return true if on windows is installed C:\\Hadoop\\hadoop-2.8.1\\bin\\WINUTILS.EXE
+     */
+    private static boolean windowsCheck() {
+        if (System.getProperty("os.name").equals("windows")) {
+            System.out.println("Hi");
+            if (new File("C:\\Hadoop\\hadoop-2.8.1").exists()) {
+                System.setProperty("hadoop.home.dir", "C:\\Hadoop");
+            } else {
+                System.out.println("Install WINUTIL.EXE from and unpack it in C:\\Windows");
+                return false;
+            }
+        }
+        return true;
     }
 
-    public static void main(String[] args) {
-        // TODO: chiamare NiFi da qui
-
-        // Must install WINUTILS.EXE for Windows :
-        // check if system is Windows
-
-//        if (System.getProperty("os.name").equals("windows")) {
-//            System.out.println("Hi");
-//            if (new File("C:\\Hadoop\\hadoop-2.8.1").exists()) {
-//                System.setProperty("hadoop.home.dir", "C:\\Hadoop");
-//            } else {
-//                System.out.println("Install WINUTIL.EXE from and unpack it in C:\\Windows");
-//                return;
-//            }
-//        }
-//        try (SparkSession spark = SparkSession
-//                .builder()
-//                .master("local")
-//                .appName("Query1")
-//                .getOrCreate();
-//             JavaSparkContext jSpark = new JavaSparkContext(spark.sparkContext())
-//        ) {
-//            System.out.println("======================= before batch =========================");
-//            // jSpark.hadoopFile(FILE_1, ParquetFileFormat.class, Integer.class, Object.class);
-//
-//            JavaRDD<Row> batch1 = spark.read().parquet(FILE_1).toJavaRDD();
-//
-//            batch1.collect().stream().map(row -> row.get(3)).forEach(System.out::println);
-//            System.out.println("======================= before passengers =========================");
-//            JavaRDD<Double> passengers = batch1.map(row -> row.getDouble(3));
-//
-//            System.out.println(passengers.collect());
-//            System.out.println("======================= before mean =========================");
-//            double mean = passengerMean(passengers);
-//            System.out.println("Mean number of passengers 2021-12: " + mean);
-//
-//            batch1.unpersist();
-//        }
-//
-//        //map + reducebykey
-//
-//        // passengers.map()
-//
-//        // .map(row -> row.get(1).toString())
-//        // .coalesce(1)
-//        // .saveAsTextFile(OUT_DIR);
-//
-//        // spark.read().parquet(FILE_1).write().mode("overwrite").parquet(OUT_DIR);
-//
-//        // spark.sparkContext().textFile(FILE_OUT, 1);
-//
-//        System.out.println("====<<<>>0>=>=>===>==== Write done ======>=>><><>>0<00>>==>=>=");
-//
-////        List<String> strings = spark.read().textFile(OUT_DIR).limit(10).collectAsList();
-////        strings.forEach(System.out::println);
-//
-//
-////        JavaRDD<String> lines = spark.read().textFile(args[0]).javaRDD();
-//
-////        JavaRDD<String> words = lines.flatMap(s -> Arrays.asList(SPACE.split(s)).iterator());
-////
-////        JavaPairRDD<String, Integer> ones = words.mapToPair(s -> new Tuple2<>(s, 1));
-////
-////        JavaPairRDD<String, Integer> counts = ones.reduceByKey((i1, i2) -> i1 + i2);
-////
-////        List<Tuple2<String, Integer>> output = counts.collect();
-////        for (Tuple2<?,?> tuple : output) {
-////            System.out.println(tuple._1() + ": " + tuple._2());
-////        }
-//
-//        /*SparkConf conf = new SparkConf()
-//                .setMaster("local")
-//                .setAppName("Query1");
-//
-//        // try-with-resources (closes automatically the spark context)
-//        try (JavaSparkContext sc = new JavaSparkContext()){
-//            System.out.println("====<<<>>0>=>=>===>==== Spark context ok ======>=>><><>>0<00>>==>=>=");
-//            // List<String> rows = spark.read().textFile(FILE_2).javaRDD().collect();
-//
-//            // spark query
-//            List<String> collect = sc.textFile(FILE_2).collect();
-//
-//            // java 8 stream API to print only 10 rows
-//            collect.stream().limit(10).forEach(System.out::println);
-//
-//            // TODO: bisogna lavorare direttamente sui parquet (o in alternativa lavorare sul CSV)
-//            // JavaRDD<String> ds = spark.read().parquet(FILE_1).javaRDD().collect();
-//        }
-//
-//         */
+    /**
+     * A working example that prints the first row of a parquet file
+     */
+    private static void workingJob(){
         try (SparkSession spark = SparkSession
                 .builder()
                 .master("spark://spark:7077")
@@ -128,81 +47,65 @@ public class Query1 {
         ) {
             // spark.sparkContext().setLogLevel("WARN");
             System.out.println("======================= before batch =========================");
-            JavaRDD<Row> batch1 = spark.read().format("parquet").load(FILE_1)
-                    .toJavaRDD();
-            System.out.println("======================= before passengers =========================");
+            JavaRDD<Row> batch1 = spark.read().format("parquet").load(FILE_1).toJavaRDD();
+            System.out.println("======================= before map =========================");
             JavaRDD<String> stringArrRDD = batch1.map(row -> {
                 StringBuilder sb = new StringBuilder();
                 int length = row.length();
                 for (int i = 0; i < length; i++) {
-                    sb.append(row.get(i) != null ? row.get(i).toString() : "");
+                    sb.append(row.get(i) != null ? row.get(i).toString() : "null").append("\t");
                 }
                 return sb.toString();
             });
 
-            stringArrRDD.collect().forEach(System.out::println);
+            System.out.println(stringArrRDD.first());
 
-
-//            System.out.println(passengers.collect());
-//            System.out.println("======================= before mean =========================");
-//            double mean = passengerMean(passengers);
-//            System.out.println("Mean number of passengers 2021-12: " + mean);
-
-            // batch1.unpersist();
+            System.out.println("====<<<>>0>=>=>===>==== Done ======>=>><><>>0<00>>==>=>=");
         }
+    }
 
-        //map + reducebykey
+    public static double passengerMean(JavaRDD<Double> passengers) {
+        passengers.cache();
+        // action that counts the number of elements in the java rdd
+        long num = passengers.count();
+        System.out.println("number of elements: " + num);
+        // action that sums each value in the rdd
+        double sum = passengers.reduce(Double::sum); // same thing as: (a, b) -> Double.sum(a, b)
+        System.out.println("number of passengers: " + sum);
+        return sum / (double) num;
+    }
 
+    public static void main(String[] args) {
+        // TODO: chiamare NiFi da qui
 
-//        passengers.map()
+        // Must install WINUTILS.EXE for Windows.
+        // windowsCheck();
 
-
-//                .map(row -> row.get(1).toString())
-//                .coalesce(1)
-//                .saveAsTextFile(OUT_DIR);
+        try (SparkSession spark = SparkSession
+                .builder()
+                .master("spark://spark:7077")
+                .appName("Query1")
+                .getOrCreate();
+        ) {
+            spark.sparkContext().setLogLevel("WARN");
+            // jSpark.hadoopFile(FILE_1, ParquetFileFormat.class, Integer.class, Object.class);
+            System.out.println("======================= before passengers =========================");
+            JavaRDD<Double> passengers = spark.read().parquet(FILE_1)
+                    .toJavaRDD()
+                    .map(row -> {
+                        try {
+                            return row.getDouble(3);
+                        } catch (NullPointerException e){
+                            return 0.0;
+                        }
+                    });
+            System.out.println("======================= before mean =========================");
+            double mean = passengerMean(passengers);
+            System.out.println("Mean number of passengers 2021-12: " + mean);
+        }
 
         // spark.read().parquet(FILE_1).write().mode("overwrite").parquet(OUT_DIR);
-
         // spark.sparkContext().textFile(FILE_OUT, 1);
 
-        System.out.println("====<<<>>0>=>=>===>==== Write done ======>=>><><>>0<00>>==>=>=");
-
-//        List<String> strings = spark.read().textFile(OUT_DIR).limit(10).collectAsList();
-//        strings.forEach(System.out::println);
-
-
-//        JavaRDD<String> lines = spark.read().textFile(args[0]).javaRDD();
-
-//        JavaRDD<String> words = lines.flatMap(s -> Arrays.asList(SPACE.split(s)).iterator());
-//
-//        JavaPairRDD<String, Integer> ones = words.mapToPair(s -> new Tuple2<>(s, 1));
-//
-//        JavaPairRDD<String, Integer> counts = ones.reduceByKey((i1, i2) -> i1 + i2);
-//
-//        List<Tuple2<String, Integer>> output = counts.collect();
-//        for (Tuple2<?,?> tuple : output) {
-//            System.out.println(tuple._1() + ": " + tuple._2());
-//        }
-
-        /*SparkConf conf = new SparkConf()
-                .setMaster("local")
-                .setAppName("Query1");
-
-        // try-with-resources (closes automatically the spark context)
-        try (JavaSparkContext sc = new JavaSparkContext()){
-            System.out.println("====<<<>>0>=>=>===>==== Spark context ok ======>=>><><>>0<00>>==>=>=");
-            // List<String> rows = spark.read().textFile(FILE_2).javaRDD().collect();
-
-            // spark query
-            List<String> collect = sc.textFile(FILE_2).collect();
-
-            // java 8 stream API to print only 10 rows
-            collect.stream().limit(10).forEach(System.out::println);
-
-            // TODO: bisogna lavorare direttamente sui parquet (o in alternativa lavorare sul CSV)
-            // JavaRDD<String> ds = spark.read().parquet(FILE_1).javaRDD().collect();
-        }
-
-         */
     }
 }
