ea041ee160ae19bfb938702776e7414c6b68f6ec
Giacomo <giacomo.redjack@gmail.com>
2022-06-13 15:56:20 +0200

    Last touches

--- src/main/java/com/sparkling_taxi/bean/QueryResult.java
+++ src/main/java/com/sparkling_taxi/bean/QueryResult.java
@@ -2,5 +2,8 @@ package com.sparkling_taxi.bean;
 
 import java.io.Serializable;
 
+/**
+ * Used to mark all QueryResult, that must also be serializable.
+ */
 public interface QueryResult extends Serializable {
 }
